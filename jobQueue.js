aed9388633be11466845cd47650a9cab145feda6
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-08-12 08:31:12 +0530

    Update jobQueue.js

--- jobQueue.js
+++ jobQueue.js
@@ -4,31 +4,33 @@ const { python, java, cpp } = require('compile-run');
 
 
 const addJobToQueue = async (filepath, language) => {
-
-  let output;
-  let startedAt;
-  if (language === "py") {
-    startedAt = Date.now();
-    output = await python.runFile(filepath, (err, result) => {
-      if (err) return err; else return result;
-
-    });
-  } else
-    if (language === "java") {
+  try {
+    let output;
+    let startedAt;
+    if (language === "py") {
       startedAt = Date.now();
-      output = await java.runFile(filepath, (err, result) => {
+      output = await python.runFile(filepath, (err, result) => {
         if (err) return err; else return result;
+
       });
     } else
-      if (language === "cpp") {
+      if (language === "java") {
         startedAt = Date.now();
-        output = await cpp.runFile(filepath, (err, result) => {
+        output = await java.runFile(filepath, (err, result) => {
           if (err) return err; else return result;
         });
-      }
+      } else
+        if (language === "cpp") {
+          startedAt = Date.now();
+          output = await cpp.runFile(filepath, (err, result) => {
+            if (err) return err; else return result;
+          });
+        }
 
-  return { startedAt: startedAt, completedAt: Date.now(), output: output, status: "success" }
-
+    return { startedAt: startedAt, completedAt: Date.now(), output: output, status: "success" };
+  } catch (err) {
+    return { startedAt: Date.now(), completedAt: Date.now(), output: JSON.stringify(err), status: "error" }
+  }
 };
 
 module.exports = {
