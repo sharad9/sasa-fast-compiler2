1aeea583cfcaafe2e2548def3a582e1d3eff9c51
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-08-13 17:21:56 +0530

    Update index.js

--- npm-compiler/index.js
+++ npm-compiler/index.js
@@ -22,25 +22,32 @@ app.post("/run", async (req, res) => {
     return res.status(400).json({ success: false, error: "Empty code body!" });
   }
   // need to generate a c++ file with content from the request
-  const filepath = await generateFile(language, code,userName);
+  const filepath = await generateFile(language, code, userName);
   // write into DB
 
-  let result = await addJobToQueue(filepath,language);
+  let result = await addJobToQueue(filepath, language);
+  console.log(result)
   const array = result.output.stdout.split('\n');
-  result.output.stdout = (result.output.stdout.substring(0,result.output.stdout.lastIndexOf('Execution Time :')))
- 
-  executionTime = (array[array.length-2]);
+  let executionTime = null;
+  if (result.output.stdout.includes('Execution Time :')) {
+    result.output.stdout = (result.output.stdout.substring(0, result.output.stdout.lastIndexOf('Execution Time :')))
+    executionTime = array[array.length - 2];
 
-  if (executionTime.startsWith('Execution Time :', 0)){
+  }
+
+
+
+  if (executionTime == null) {
+    res.status(201).json({ result: result, executionTime: 'Execution Time : No_result' });
+
+  } else {
+
     res.status(201).json({ result: result, executionTime: executionTime });
-  }else{
-    
-  res.status(201).json({result:result,executionTime:'Execution Time : No_result'});
   }
 });
 
 
 
 app.listen(process.env.PORT || 5000, () => {
-	console.log(`Listening on port 5000!`);
+  console.log(`Listening on port 5000!`);
 });
